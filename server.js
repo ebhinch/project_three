cda30333ecd525d29bb13e97c93e95c5ec3daa02
ebhinch <erica.hinchman@gmail.com>
2017-10-13 15:35:17 -0400

    get route at the index that will serve the index.html of the built production React app

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ connection.on('error', (err) => {
 }); 
 
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/client/build/'));
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+  })
+
+
 app.get('/', (req,res) => {
   res.send('Hello world!')
 })
