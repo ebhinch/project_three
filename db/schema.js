3d8a8232494f4a7aa49e29992993b840ab2bd17c
ebhinch <erica.hinchman@gmail.com>
2017-10-13 17:24:36 -0400

    seeds and schema built out. seeds tested with mongo

---
+++ db/schema.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose")
+
+
+//define schema for each model
+// const restaurantSchema = mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     website: {
+//         type: String,
+//         required: true
+//     }
+
+// })
+
+const wineSchema = mongoose.Schema({
+    name: {
+        type: String,
+        default: "New Wine Name"
+    },
+    vintage: {
+        type: String,
+        default: "New Wine Vintage"
+    },
+    description: {
+        type: String,
+        default: "New Wine Description"
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    image: {
+        type: String,
+        default: "New Wine Image"
+    }
+})
+
+const vineyardSchema = mongoose.Schema({
+    name: {
+        type: String,
+        default: "New Winery Name"
+    },
+    address: {
+        type: String,
+        default: "New Winery Address"
+    },
+    website: {
+        type: String,
+        default: "New Winery URL"
+    },
+    description: {
+        type: String,
+        default: "New Winery Description"
+    },
+    wines: [wineSchema]
+    // , restaurants: [restaurantSchema]
+})
+
+
+const userSchema = mongoose.Schema({
+    userName: String,
+    name: String,
+    hometown: String,
+    season: String
+})
+
+// const RestaurantModel = mongoose.model("Restaurant", restaurantSchema)
+const Vineyard = mongoose.model("Vineyard", vineyardSchema)
+const Wine = mongoose.model("Wine", wineSchema)
+const User = mongoose.model("User", userSchema)
+
+module.exports = {
+    // RestaurantModel: RestaurantModel,
+    Vineyard, Wine, User
+}
---
+++ db/seeds.js
@@ -0,0 +1,88 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true })
+mongoose.Promise = global.Promise
+
+const { Vineyard, Wine, User } = require("./schema.js")
+
+const jeffersonMeritage = new Wine({
+    name: "Meritage",
+    vintage: "2015",
+    description: "A cedar and blackberry nose followed by a bright black cherry palate, ending with balanced spice",
+    price: 31.95,
+    image: "https://scontent-atl3-1.xx.fbcdn.net/v/t31.0-8/20626121_10155404351165851_9075711873478245884_o.jpg?oh=b015976b1f7a0ce701387a41d7061bc3&oe=5A8621CF"
+})
+
+const kingCrose = new Wine({
+    name: "",
+    vintage: "NV",
+    description: "A dry rosé named after our small town of Crozet. ",
+    price: 19,
+    image: "http://www.kingfamilyvineyards.com/assets/images/products/pictures/crose.jpg"
+})
+
+const aftonFesta = new Wine({
+    name: "Festa di Bacco",
+    vintage: "2014",
+    description: "Indicative blend: Sangiovese, Cabernet Franc, Merlot, and Petit Verdot.",
+    price: 28,
+    image: "https://i.pinimg.com/736x/ab/71/ba/ab71ba3ca986d0882a241782796cda7f--road-trippin-wineries.jpg"
+})
+
+const jefferson = new Vineyard({
+    name: "Jefferson Vineyards",
+    address: "1353 Thomas Jefferson Parkway Charlottesville, Virginia 22902",
+    website: "http://jeffersonvineyards.com/",
+    description: "Jefferson Vineyards is a family owned winery and vineyard in Charlottesville, Virginia, located where Thomas Jefferson and Philip Mazzei first began the American wine revolution.",
+    wines: [jeffersonMeritage]
+})
+
+const afton = new Vineyard({
+    name: "Afton Mountain Vineyards",
+    address: "234 Vineyard Lane, Afton, Virginia 22920",
+    website: "http://www.aftonmountainvineyards.com/",
+    description: "Afton Mountain Vineyards is one of Virginia's first farm wineries; the original vines were planted in 1978. ",
+    wines: [aftonFesta]
+})
+
+const king = new Vineyard({
+    name: "King Family Vineyards",
+    address: "6550 Roseland Farm Crozet, VA 22932",
+    website: "http://www.kingfamilyvineyards.com/",
+    description: "King Family Vineyards is a family owned and operated winery located at the foothills of the Blue Ridge Mountains in Crozet, Virginia just fifteen minutes from Charlottesville.",
+    wines: [kingCrose]
+})
+
+const victoria = new User({
+    userName: "vicky.t",
+    name: "Victoria",
+    hometown: "Atlanta",
+    season: "Spring 2018"
+})
+
+const skylar = new User({
+    userName: "skyguy",
+    name: "Skylar",
+    hometown: "Asheville",
+    season: "Winter 2017"
+})
+
+const hunter = new User({
+    userName: "the.mad.duke",
+    name: "Hunter",
+    hometown: "Decatur",
+    season: "Fall 2018"
+})
+
+User.remove({})
+    .then(() => hunter.save())
+    .then(() => skylar.save())
+    .then(() => victoria.save())
+    .then(() => console.log("Successful Saved Users"))
+
+Vineyard.remove({})
+    .then(() => king.save())
+    .then(() => afton.save())
+    .then(() => jefferson.save())
+    .then(() => console.log("Successful Saved Vineyards"))
+    .then(() => mongoose.connection.close())
